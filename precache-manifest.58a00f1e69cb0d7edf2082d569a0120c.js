a84d5d22caf858f34534d34f6198724bf25f3de6
juandara <juan.ramirez.villegas@correounivalle.edu.co>
2020-08-10 13:49:19 -0500

    Updates

---
+++ precache-manifest.58a00f1e69cb0d7edf2082d569a0120c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5b477afec3c82cd7442fb0198c09083",
+    "url": "./index.html"
+  },
+  {
+    "revision": "36a33300a92fd60c83b3",
+    "url": "./static/css/main.d65db33c.chunk.css"
+  },
+  {
+    "revision": "a1a59fe500095f0e707f",
+    "url": "./static/js/2.e0df9d47.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "./static/js/2.e0df9d47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36a33300a92fd60c83b3",
+    "url": "./static/js/main.9b60204d.chunk.js"
+  },
+  {
+    "revision": "c3f8d861af710f8fce0a",
+    "url": "./static/js/runtime-main.59c85f99.js"
+  },
+  {
+    "revision": "fae7a54c508c5a34c240c01c0585bb8c",
+    "url": "./static/media/personal_logo.fae7a54c.png"
+  }
+]);
