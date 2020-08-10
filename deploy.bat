4424c86c17010ab4ee0eb4b3b3a035539a9f8e02
juandara <juan.ramirez.villegas@correounivalle.edu.co>
2020-08-10 13:03:43 -0500

    heroku deploy version

--- deploy.bat
+++ deploy.bat
@@ -5,4 +5,4 @@
 npm publish --access public
 typedoc --out ./documentation/ src
 :: upload to aws s3 bucket the documentation
-git subtree push --prefix path/to/subdirectory heroku master
+git subtree push --prefix example heroku master
