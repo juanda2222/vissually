bbf9313157151b4427d11303d0daea96737b7a06
juandara <juan.ramirez.villegas@correounivalle.edu.co>
2020-08-27 05:36:07 -0500

    npm api changed to env var

--- deploy.bat
+++ deploy.bat
@@ -1,5 +1,5 @@
 
 ::hipotetical deploy steps from a local computer
 ::npm publish --access public
-:: typedoc --out ./documentation/ src --exclude "**/*+(test|index).tsx" --allowJs
+:: typedoc --out ./documentation/ src --exclude "**/*+(test|index).tsx" --allowJs --ignoreCompilerErrors
 :: upload to aws s3 bucket the documentation
