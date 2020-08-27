76c5bceab69ed263de143da75122595a6a9ccf7f
juandara <juan.ramirez.villegas@correounivalle.edu.co>
2020-08-27 03:12:33 -0500

    before deploy (automatic npm and s3 deploy added)

--- deploy.bat
+++ deploy.bat
@@ -1,7 +1,6 @@
 
 ::hipotetical deploy steps from a local computer
 
-:: script to create the code files
 npm publish --access public
 typedoc --out ./documentation/ src
 :: upload to aws s3 bucket the documentation
